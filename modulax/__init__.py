f3d4ef38a04fdb2d3d3e8137718f4e67a75ae105
jack <jack@gallabytes.com>
2024-08-07 20:09:45 +0000

    __init__ exports stuff

---
+++ modulax/__init__.py
@@ -0,0 +1,19 @@
+from .abstract import (
+    Add,
+    CompositeModule,
+    DictModule,
+    Module,
+    Mul,
+    Pow,
+    Prod,
+    ShardingConfig,
+    ShardingMode,
+    Sum,
+    TupleModule,
+    VMap,
+    WrapperModule,
+    with_sharding_config,
+    with_sharding_mode,
+)
+from .atom import *
+from .bond import *
